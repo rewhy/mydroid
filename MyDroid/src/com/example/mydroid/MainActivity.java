219c4d186d71b27a65a555c9afd85ea62e72518d
enggarmanah <radixe@yahoo.com>
2014-10-24 18:11:55 +0800

    Intent update

--- MyDroid/src/com/example/mydroid/MainActivity.java
+++ MyDroid/src/com/example/mydroid/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.mydroid;
 
+import com.example.mydroid.DisplayMessageActivity;
+import com.example.mydroid.R;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	
+	public final static String EXTRA_MESSAGE = "com.poc.mydroid.MESSAGE";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,5 +38,17 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+	    // Do something in response to button
+		
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+	    EditText editText = (EditText) findViewById(R.id.edit_message);
+	    String message = editText.getText().toString();
+	    intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
+
 	}
 }
